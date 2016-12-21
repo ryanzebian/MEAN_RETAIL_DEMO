cca4389e216d694e15345be58949ec8a7060f35c
ryanzebian <zebian93@gmail.com>
2016-12-21 02:10:10 +0200

    Added Facebook Auth

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('./schemas/model')(wagner);
 
 var app = express();
 
+wagner.invoke(require('./auth'),{app:app});
+
 app.use('/api/v1/', require('./api')(wagner));
 app.listen(3000);
 console.log('Listening to Port 3000!');
